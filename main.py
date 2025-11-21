222729b07da65b44967450bbdf99ec23bff4eca1
biplovgautam <madhavbiplov@gmail.com>
2025-11-21 19:09:23 +0545

    tittle changed

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ all the routers defined in app/api/endpoints.py.'''
 from fastapi import FastAPI
 from app.api import endpoints
 
-app = FastAPI() 
+app = FastAPI(
+    title="RAGtask API",
+    description="This is a custom RAG developed for my AI/ML intern task at palm mind.",
+    version="1.0.0") 
 
 
 # includes all endpoints in this actual app from the app/api/endpoints.py
